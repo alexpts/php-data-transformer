9ab091fdb8787dfab0791f829dfb2cc3253f4e7f
alexpts <alexpts@yandex.ru>
2016-01-09 19:33:42 +0300

    init repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# php-data-transformer
+
+[![Build Status](https://travis-ci.org/alexpts/php-data-transformer.svg?branch=master)](https://travis-ci.org/alexpts/php-data-transformer)
+[![Test Coverage](https://codeclimate.com/github/alexpts/php-data-transformer/badges/coverage.svg)](https://codeclimate.com/github/alexpts/php-data-transformer/coverage)
+[![Code Climate](https://codeclimate.com/github/alexpts/php-data-transformer/badges/gpa.svg)](https://codeclimate.com/github/alexpts/php-data-transformer)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/alexpts/php-data-transformer/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/alexpts/php-data-transformer/?branch=master)
+
---
+++ src/PTS/DataTransformer/MapsManager.php
@@ -0,0 +1,89 @@
+<?php
+namespace PTS\DataTransformer;
+
+use \Exception;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Parser as YamlParser;
+
+class MapsManager
+{
+    /** @var YamlParser */
+    protected $yamlParser;
+    /** @var array */
+    protected $cache = [];
+    /** @var array */
+    protected $mapsDirs = [];
+
+    /**
+     * @param YamlParser $parser
+     */
+    public function __construct(YamlParser $parser)
+    {
+        $this->yamlParser = $parser;
+    }
+
+    /**
+     * @param string $name
+     * @param string $dir
+     * @throws Exception
+     */
+    public function setMapDir($name, $dir)
+    {
+        $this->mapsDirs[$name] = $dir;
+    }
+
+    /**
+     * @param string $name
+     * @param string $type
+     * @return array
+     *
+     * @throws ParseException
+     */
+    public function getMap($name, $type = 'dto')
+    {
+        $map = $this->tryCache($name, $type);
+        if ($map) {
+            return $map;
+        }
+
+        $dir = $this->mapsDirs[$name];
+        $map = $this->getByPath($dir . '/' . $type . '.yml');
+
+        $this->setCache($name, $type, $map);
+        return $map;
+    }
+
+    /**
+     * @param string $name
+     * @param string $type
+     * @param array $map
+     */
+    protected function setCache($name, $type, array $map)
+    {
+        $this->cache[$name][$type] = $map;
+    }
+
+    /**
+     * @param string $name
+     * @param string $type
+     * @return array|null
+     */
+    protected function tryCache($name, $type)
+    {
+        if (isset($this->cache[$name], $this->cache[$name][$type])) {
+            return $this->cache[$name][$type];
+        }
+
+        return null;
+    }
+
+    /**
+     * @param string $path
+     * @return array
+     * @throws ParseException
+     */
+    protected function getByPath($path)
+    {
+        return $this->yamlParser->parse(file_get_contents($path));
+    }
+}
