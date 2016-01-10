07d150d7767f1b4113ec880659f26de9886700ff
alexpts <alexpts@yandex.ru>
2016-01-10 12:59:51 +0300

    refactoring

--- src/PTS/DataTransformer/ModelClosure.php
+++ src/PTS/DataTransformer/ModelClosure.php
@@ -6,41 +6,18 @@ class ModelClosure
     /** @var \Closure */
     protected $fillFn;
     /** @var \Closure */
-    protected $toDataFn;
+    protected $dataFn;
 
     /**
      * @return \Closure
      */
     public function getToDataFn()
     {
-        if (!$this->toDataFn) {
-            $this->toDataFn = function (array $mapping, TypeConverter $typeConverter) {
-                $props = [];
-
-                foreach ($mapping as $name => $prop) {
-                    $getter = isset($prop['get']) ? $prop['get'] : null;
-                    $propVal = isset($prop['prop']) ? $prop['prop'] : null;
-
-                    if ($getter) {
-                        $method = is_array($getter) ? $getter[0] : $getter;
-                        $args = is_array($getter) ? $getter[1] : [];
-                        $val = call_user_func_array([$this, $method], $args);
-                    } elseif ($propVal) {
-                        $val = $this->{$propVal};
-                    } else {
-                        throw new \InvalidArgumentException('Bad mapping config');
-                    }
-
-                    if ($val !== null) {
-                        $props[$name] = $typeConverter->toStorage($val, $prop);
-                    }
-                }
-
-                return $props;
-            };
+        if (!$this->dataFn) {
+            $this->dataFn = $this->createDataFn();
         }
 
-        return $this->toDataFn;
+        return $this->dataFn;
     }
 
     /**
@@ -49,27 +26,64 @@ class ModelClosure
     public function getFillFn()
     {
         if (!$this->fillFn) {
-            $this->fillFn = function (array $doc, array $mapping, TypeConverter $typeConverter) {
-
-                foreach ($doc as $name => $val) {
-                    if (!array_key_exists($name, $mapping)) {
-                        continue;
-                    }
-
-                    $prop = $mapping[$name];
-
-                    $modelName = array_key_exists('prop', $prop) ? $prop['prop'] : $name;
-                    $setter = array_key_exists('set', $prop) ? $prop['set'] : null;
-
-                    $val = $typeConverter->toModel($val, $prop);
-
-                    $setter
-                        ? call_user_func([$this, $setter], $val)
-                        : $this->{$modelName} = $val;
-                }
-            };
+            $this->fillFn = $this->createFillFn();
         }
 
         return $this->fillFn;
+    }
+
+    /**
+     * @return \Closure
+     */
+    protected function createDataFn() {
+        return function (array $mapping, TypeConverter $typeConverter) {
+            $props = [];
+
+            foreach ($mapping as $name => $prop) {
+                $getter = isset($prop['get']) ? $prop['get'] : null;
+                $propVal = isset($prop['prop']) ? $prop['prop'] : null;
+
+                if ($getter) {
+                    $method = is_array($getter) ? $getter[0] : $getter;
+                    $args = is_array($getter) ? $getter[1] : [];
+                    $val = call_user_func_array([$this, $method], $args);
+                } elseif ($propVal) {
+                    $val = $this->{$propVal};
+                } else {
+                    throw new \InvalidArgumentException('Bad mapping config');
+                }
+
+                if ($val !== null) {
+                    $props[$name] = $typeConverter->toStorage($val, $prop);
+                }
+            }
+
+            return $props;
+        };
+    }
+
+    /**
+     * @return \Closure
+     */
+    protected function createFillFn()
+    {
+        return function(array $doc, array $mapping, TypeConverter $typeConverter) {
+            foreach ($doc as $name => $val) {
+                if (!array_key_exists($name, $mapping)) {
+                    continue;
+                }
+
+                $prop = $mapping[$name];
+
+                $modelName = array_key_exists('prop', $prop) ? $prop['prop'] : $name;
+                $setter = array_key_exists('set', $prop) ? $prop['set'] : null;
+
+                $val = $typeConverter->toModel($val, $prop);
+
+                $setter
+                    ? call_user_func([$this, $setter], $val)
+                    : $this->{$modelName} = $val;
+            }
+        };
     }
 }
